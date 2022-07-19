02b97f97a1b8ee7bc43bc5aa6913d830340427fb
Connor <Woodconnor13@gmail.com>
2022-07-19 16:46:14 -0400

    Finished movie lab

---
+++ MovieDB/Models/Movie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieDB.Models
+{
+    public partial class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Genre { get; set; }
+
+        //methods
+        //searching by genre
+        public static void SearchByGenre(string genre)
+        {
+            using (MovieDBContext context = new MovieDBContext())
+            {
+                List<Movie> matchingGenre = context.Movies.Where(m => m.Genre.ToLower().Contains(genre)).ToList();
+                foreach (Movie movie in matchingGenre)
+                {
+                    Console.WriteLine($"{movie.Title} is a {movie.Genre} type of film.");
+                }
+            }
+        }
+        //searching by title
+        public static void SearchByTitle(string title)
+        {
+            using (MovieDBContext context = new MovieDBContext())
+            {
+                List<Movie> matchingTitle = context.Movies.Where(m => m.Title.ToLower().Contains(title)).ToList();
+                foreach (Movie movie in matchingTitle)
+                {
+                    Console.WriteLine($"{movie.Title} is a {movie.Genre} type of film.");
+                }
+            }
+        }
+
+    }
+}
---
+++ MovieDB/Program.cs
@@ -0,0 +1,107 @@
+﻿using MovieDB;
+using MovieDB.Models;
+
+
+//main program
+bool choice;
+Console.WriteLine("Welcome to BlockBuster! We offer a wide selection of movies!");
+choice = GenreOrTitle();
+string title = "";
+string genre = "";
+
+if (choice)
+{
+    Console.WriteLine("Please enter the title that you would like to search for.");
+    title = Console.ReadLine().ToLower().Trim();
+    Movie.SearchByTitle(title);
+}
+else
+{
+    Console.WriteLine("Please enter the genre that you would like to search for.");
+    genre = Console.ReadLine().ToLower().Trim();
+    Movie.SearchByGenre(genre);
+}
+
+
+
+
+
+
+
+
+//List<Movie> selection = new List<Movie>()
+//{
+//    new Movie()
+//    {Title = "Jurassic Park",
+//    Genre =  "Scifi"
+//    },   
+//    new Movie()
+//    {Title = "Basket Case",
+//    Genre =  "Horror"
+//    },
+//    new Movie()
+//    {Title = "The Matrix",
+//    Genre = "Scifi"
+//    },
+//    new Movie()
+//    {Title = "The Godfather",
+//    Genre = "Crime"
+//    },
+//    new Movie()
+//    {Title = "The Faculty", 
+//    Genre = "Horror"
+//    },
+//    new Movie()
+//    {Title = "Friday The 13th",
+//    Genre = "Horror"
+//    },
+//    new Movie()
+//    {Title = "Minions: The Rise of Gru",
+//    Genre ="Comedy"
+//    },
+//    new Movie()
+//    {Title = "Forrest Gump",
+//    Genre =  "Drama"
+//    },
+//    new Movie()
+//    {Title = "Shallow Hal", 
+//    Genre = "Comedy"
+//    },
+//    new Movie()
+//    {Title = "Shrek", 
+//    Genre = "Animated"
+//    },
+//    new Movie()
+//    {
+//        Title = "A Silent Voice", 
+//        Genre = "Anime"
+//    }
+//};
+
+//MovieAdder(selection);
+
+//methods
+//static void MovieAdder(List<Movie> movies)
+//{
+//    using (MovieDBContext context = new MovieDBContext())
+//    {
+//        for (int i = 0; i < movies.Count; i++)
+//        {
+//            context.Movies.Add(movies[i]);
+//        }
+//        context.SaveChanges();
+//    }
+//}
+
+static bool GenreOrTitle()
+{
+    bool result;
+    result = Validator.Validator.GetContinue("Would you like to by title or by genre?", "title", "genre");
+    return result;
+}
+
+
+
+
+
+
